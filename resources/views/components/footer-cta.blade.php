6cb313cef176ee28b6124339a3d1f14ec56024c1
Lan <truongylan2000@gmail.com>
2021-03-04 10:23:04 +0700

    #1 Add Footer CTA

---
+++ resources/views/components/footer-cta.blade.php
@@ -0,0 +1,15 @@
+<div class="footer__cta">
+  <div class="small-container">
+    <h1 class="heading heading-h1 footer__cta--heading">Lorem ipsum dolor sit amet consectetur adipiscing elit</h1>
+
+    <p class="footer__cta--para">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.</p>
+
+    <p class="footer__cta--para">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit.</p>
+
+    <p class="footer__cta--para">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+
+    <div class="footer__cta--btn font-montserrat">
+      <a href="">Click me</a>
+    </div>
+  </div>
+</div>
