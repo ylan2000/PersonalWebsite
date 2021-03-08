9ba0e34e364c569e07bf271d35f567f4e84603e7
Lan <truongylan2000@gmail.com>
2021-03-08 17:37:33 +0700

    #7 Create form sign up recommend component

--- resources/views/pages/newsletters.blade.php
+++ resources/views/pages/newsletters.blade.php
@@ -8,18 +8,7 @@
   <h1 class="heading heading-primary text-center">Lorem ipsum</h1>
   <p class="newsletters__desc">The latest newsletter can be read by anyone. Previous newsletters can be read by site members. <a href="" class="link hover-underline">Learn more about the site membership.</a></p>
 
-  <div class="sign-up-recommend text-center">
-    <h2 class="heading heading-secondary">Sign Up to Receive More Book Recommendations</h2>
-
-    <form action="" class="form-recommend">
-      <input type="text" placeholder="Your Email Address">
-      <input type="submit" value="SIGN UP">
-    </form>
-
-    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-    <p>Note: Your information is protected and I never spam, ever. You can view my privacy policy <a href="" class="link">here</a> .</p>
-  </div>
+  @include('components/form-sign-up-recommend')
 
   <table class="newsletters__topic">
     <tbody>
